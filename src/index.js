6e710732ea4ddae4be120c1228653b3505055ebc
dand <79340018+dZyad@users.noreply.github.com>
2024-04-08 12:47:45 -0300

    Add home

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
-console.log('Hello world!');
+const content = document.querySelector('#content');
+
+function addHome() {
+    const title = document.createElement('h1');
+    const presentation = document.createElement('p');
+
+    title.textContent = 'Minimalist';
+    presentation.innerHTML = `Welcome to <em>minimalist</em>. A place where <em>less is <strong>more</strong></em>.`
+
+    content.append(title);
+    content.append(presentation);
+}
+
+addHome();
