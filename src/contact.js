f7b7cde9eddca893cab848187e029fb5dce377b2
dand <79340018+dZyad@users.noreply.github.com>
2024-04-08 19:02:27 -0300

    Make mobile compatible contact page

--- src/contact.js
+++ src/contact.js
@@ -2,12 +2,13 @@ export const contactPage = function() {
     const content = document.querySelector('#content');
     const title = document.createElement('h1');
     const info = document.createElement('p');
+    const extra = document.createElement('p');
 
     title.textContent = 'Contact.';
-    info.setAttribute('style', 'white-space: pre;');
-    info.textContent = 'Request your reservation to thisisnotareal@email.com.\r\n';
-    info.textContent += 'Address and instructions will be provided with your reservation.';
+    info.textContent = 'Request your reservation to thisisnotareal@email.com.';
+    extra.textContent = 'Address and instructions will be provided with your reservation.';
 
     content.append(title);
     content.append(info);
+    content.append(extra);
 }
