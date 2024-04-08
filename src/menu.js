882bd6774c7aea45667594694984d1f8b2c6d573
dand <79340018+dZyad@users.noreply.github.com>
2024-04-08 18:04:40 -0300

    Add photo to menu

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,5 @@
+import food from './media/food.jpg';
+
 export const menuPage = function() {
 
     const content = document.querySelector('#content');
@@ -7,7 +9,10 @@ export const menuPage = function() {
     title.textContent = 'Menu.';
     menuInfo.textContent = 'Our meals are unique and our chef picks it with fresh ingredients. ';
     menuInfo.textContent += 'When requesting a reservation you will be asked about your food restrictions';
+    const myPic = new Image(300);
+    myPic.src = food;
 
     content.append(title);
     content.append(menuInfo);
+    content.append(myPic);
 }
